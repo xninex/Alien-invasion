05e9fd6cdcf3c329f109feffe4004fe2ae1a1ca1
xninex <a-olovyannyy@mail.ru>
2022-08-31 14:58:23 +0300

    move_ship

---
+++ settings.py
@@ -0,0 +1,10 @@
+import pygame.image
+
+class Settings():
+    def __init__(self):
+        self.screen_width = 1800
+        self.screen_height = 1000
+        self.bg_color = (48, 48, 48)
+        # self.bg = pygame.image.load('images/fon.bmp')
+        self.ship_speed = 1.5
+
---
+++ ship.py
@@ -0,0 +1,42 @@
+import pygame
+
+
+class Ship():
+    def __init__(self, ai_game):
+
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        self.image = pygame.image.load('images/my_ship.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        #Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+
+    def update(self):
+        #Обновляет позицию корабля с учетом флага
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.y > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.y < 940:
+            self.y += self.settings.ship_speed
+
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+
